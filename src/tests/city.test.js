d268da71b7d2b1e838b672cd2d2338e182a9085a
Andres Mendoza <andres.david.mm@hotmail.com>
2023-11-14 22:49:54 -0500

    first commit

--- src/tests/city.test.js
+++ src/tests/city.test.js
@@ -1,6 +1,7 @@
 const request = require('supertest');
 const app = require('../app');
 
+let id;
 
 test("GET /cities debe traer las ciudades", async () => {
     const res = await request(app).get('/cities');
@@ -15,7 +16,20 @@ test("POST /cities debe crear una ciudad", async () => {
         isCapital: false,
     }
     const res = await request(app).post('/cities').send(city);
+    id = res.body.id;
     expect(res.status).toBe(201);
     expect(res.body.id).toBeDefined();
     expect(res.body.name).toBe(city.name);
+});
+
+test("PUT /cities/:id debe actualizar una ciudad", async () => {
+    const city = { name: "Medellin actualizada" }
+    const res = await request(app).put(`/cities/${id}`).send(city);
+    expect(res.status).toBe(200);
+    expect(res.body.name).toBe(city.name);
+});
+
+test("DELETE /cities/:id debe eliminar una ciudad", async () => {
+    const res = await request(app).delete(`/cities/${id}`);
+    expect(res.status).toBe(204);
 });
--- src/tests/user.test.js
+++ src/tests/user.test.js
@@ -1,6 +1,7 @@
 const request = require('supertest');
 const app = require('../app');
 
+let id;
 
 test("GET /users debe traer los usuarios", async () => {
     const res = await request(app).get('/users');
@@ -16,7 +17,22 @@ test("POST /users debe crear un usuario", async() => {
         email: "rodrigo@gmail.com",
     }
     const res = await request(app).post('/users').send(user)
+    id = res.body.id;
     expect(res.status).toBe(201);
     expect(res.body.id).toBeDefined();
     expect(res.body.firstName).toBe(user.firstName)
+});
+
+test("PUT /users/:id debe actualizar un usuario", async () => {
+    const user = {
+        firstName: "Rodrigo actualizado",
+    }
+    const res = await request(app).put(`/users/${id}`).send(user);
+    expect(res.status).toBe(200);
+    expect(res.body.firstName).toBe(user.firstName);
+});
+
+test("DELETE /users/:id debe eliminar un usuario", async() => {
+    const res = await request(app).delete(`/users/${id}`);
+    expect(res.status).toBe(204);
 });
